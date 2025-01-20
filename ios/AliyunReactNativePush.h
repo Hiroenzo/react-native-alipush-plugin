eff5d96f636bb844119e7ef1e8a9830a1ad3e6a9
Kyrie <kyriechung@outlook.com>
2025-01-20 10:59:14 +0800

    fix: 修复ios头文件引用地址错误的问题

--- ios/AliyunReactNativePush.h
+++ ios/AliyunReactNativePush.h
@@ -1,19 +1,21 @@
-
 // iOS 10 notification
 #import <UserNotifications/UserNotifications.h>
 #import <React/RCTEventEmitter.h>
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNAliyunReactNativePushSpec.h"
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <RNAliyunReactNativePushSpec/RNAliyunReactNativePushSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface AliyunPush : RCTEventEmitter <NativeAliyunReactNativePushSpec>
+
+NS_ASSUME_NONNULL_END
+
 #else
 #import <React/RCTBridgeModule.h>
 
-
 @interface AliyunPush : RCTEventEmitter <RCTBridgeModule>
 #endif
-
 
 typedef void (^AliyunPushRemoteNotificationCallback)(UIBackgroundFetchResult result);
 typedef void (^AliyunPushForeReceiveNoticeCallback)(UNNotificationPresentationOptions);
